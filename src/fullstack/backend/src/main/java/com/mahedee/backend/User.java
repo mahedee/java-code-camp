583a42b383ff655f32e7c1c25567bc1aa0d2911e
mahedee <mahedee.hasan@gmail.com>
2023-08-11 15:48:51 -0300

    ....

--- src/fullstack/backend/src/main/java/com/mahedee/backend/User.java
+++ src/fullstack/backend/src/main/java/com/mahedee/backend/User.java
@@ -3,10 +3,14 @@ package com.mahedee.backend;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
--- src/fullstack/backend/src/main/java/com/mahedee/backend/UserController.java
+++ src/fullstack/backend/src/main/java/com/mahedee/backend/UserController.java
@@ -3,6 +3,7 @@ package com.mahedee.backend;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class UserController {
 
     @Autowired
