537bee210fe2fda65e00d1e063d78e7f5926830a
mahedee <mahedee.hasan@gmail.com>
2023-09-19 17:30:09 -0300

    ...

--- src/auth-springboot-react/README.md
+++ src/auth-springboot-react/README.md
@@ -1,12 +1,11 @@
-## Note
+## How to run application
 
-- How to run application
-
-* Create a database like: testdb
-* Go to the directory whre pom.xml file exists
-* Run mvn spring-boot:run
-* Now see corresponding table is created already
-* Now insert the following data in role table
+- **Backend**
+- Create a database like: testdb
+- Go to the directory whre pom.xml file exists
+- Run mvn `spring-boot:run`
+- Now see corresponding table is created already
+- Now insert the following data in role table
 
 ```
 INSERT INTO roles(name) VALUES('ROLE_USER');
@@ -14,14 +13,19 @@ INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 ```
 
+- Sample user: admin, password: mahedee.net
+
 - Swagger: http://localhost:8080/swagger-ui/index.html
+
+## Note
 
 ## References
 
+- https://github.com/bezkoder/spring-boot-spring-security-jwt-authentication
+- https://www.bezkoder.com/react-hooks-redux-login-registration-example/
 - https://springjava.com/spring-boot/how-to-create-a-spring-boot-project-in-vs-code
 - https://www.youtube.com/watch?v=CsgtYvlR7xk
 - https://www.bezkoder.com/spring-boot-jwt-authentication/
-- https://github.com/bezkoder/spring-boot-spring-security-jwt-authentication
 - https://github.com/bezkoder/react-jwt-auth
 - https://tariqul-islam-rony.medium.com/spring-boot-with-visual-studio-code-visual-studio-code-part-2-7943febb52f8
 - [How to import and debug a Spring Boot Java Maven project in VS Code](https://www.youtube.com/watch?v=XJeT0ErXBHo)
---
+++ src/auth-springboot-react/frontend/client-app/src/redux/actions/auth.js
@@ -0,0 +1,85 @@
+import AuthService from "../../services/auth.servic";
+import {
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  SET_MESSAGE,
+} from "./type";
+
+export const register = (username, email, password) => (dispatch) => {
+  return AuthService.register(username, email, password).then(
+    (response) => {
+      dispatch({
+        type: REGISTER_SUCCESS,
+      });
+
+      dispatch({
+        type: SET_MESSAGE,
+        payload: response.data.message,
+      });
+
+      return Promise.resolve();
+    },
+    (error) => {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      dispatch({
+        type: REGISTER_FAIL,
+      });
+
+      dispatch({
+        type: SET_MESSAGE,
+        payload: message,
+      });
+
+      return Promise.reject();
+    }
+  );
+};
+
+//This line defines an action creator function called login.
+//It takes two parameters, username and password, and returns a function that takes dispatch as its argument
+
+export const login = (username, password) => (dispatch) => {
+  return AuthService.login(username, password).then(
+    (data) => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: { user: data },
+      });
+      return Promise.resolve();
+    },
+    (error) => {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      dispatch({
+        type: LOGIN_FAIL,
+      });
+
+      dispatch({
+        type: SET_MESSAGE,
+        payload: message,
+      });
+
+      return Promise.reject();
+    }
+  );
+};
+
+export const logout = () => (dispatch) => {
+  AuthService.logout();
+  dispatch({
+    type: LOGOUT,
+  });
+};
