b3e3017d953155d8320cf71804e3803753b30fb8
mahedee <mahedee.hasan@gmail.com>
2024-09-15 16:58:03 -0400

    ....

---
+++ rnd/src/PGP/pgp-encryptor/pgpdemo/src/main/java/com/mahedee/App.java
@@ -0,0 +1,14 @@
+package com.mahedee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        CommonUtils.displayName();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rnd/src/PGP/pgp-encryptor/pgpdemo/src/main/java/com/mahedee/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.mahedee;
+
+class CommonUtils {
+
+    public static void displayName()
+    {
+        System.out.println("This is the method: CommonUtils -> displayName");
+    }
+    
+}
