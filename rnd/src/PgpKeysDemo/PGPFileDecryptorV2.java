ec799bce4b046fb751076db002b38c845944aa82
mahedee <mahedee.hasan@gmail.com>
2024-09-15 11:31:11 -0400

    one project in pgp working fine.

--- rnd/src/PgpKeysDemo/PGPFileDecryptorV2.java
+++ rnd/src/PgpKeysDemo/PGPFileDecryptorV2.java
@@ -1,10 +1,12 @@
 import org.bouncycastle.bcpg.ArmoredInputStream;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.jcajce.JcaPGPObjectFactory;
 import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
 import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyDataDecryptorFactoryBuilder;
 import org.bouncycastle.openpgp.operator.PBESecretKeyDecryptor;
 import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
+import org.bouncycastle.util.io.Streams;
 
 import java.io.*;
 import java.security.Security;
@@ -25,9 +27,11 @@ public class PGPFileDecryptorV2 {
              InputStream decoderStream = PGPUtil.getDecoderStream(fileInput);
              FileOutputStream fos = new FileOutputStream(outputFilePath)) {
 
-            PGPObjectFactory pgpFactory = new PGPObjectFactory(decoderStream, new JcaKeyFingerprintCalculator());
+            // Use JcaPGPObjectFactory for parsing PGP objects
+            JcaPGPObjectFactory pgpFactory = new JcaPGPObjectFactory(decoderStream);
             Object object = pgpFactory.nextObject();
 
+            // Handle PGPEncryptedDataList
             if (object instanceof PGPEncryptedDataList) {
                 PGPEncryptedDataList encryptedDataList = (PGPEncryptedDataList) object;
                 decryptData(encryptedDataList, privateKey, fos);
@@ -64,7 +68,6 @@ public class PGPFileDecryptorV2 {
         }
     }
 
-    // Updated decryptPrivateKey method
     private static PGPPrivateKey decryptPrivateKey(PGPSecretKey secretKey, String passphrase) throws PGPException {
         if (secretKey == null) {
             throw new IllegalArgumentException("Secret key is null");
@@ -77,75 +80,57 @@ public class PGPFileDecryptorV2 {
         return secretKey.extractPrivateKey(decryptorFactory);
     }
 
-    
-
     private static void decryptData(PGPEncryptedDataList encryptedDataList, PGPPrivateKey privateKey, OutputStream outStream) throws IOException, PGPException {
         PGPPublicKeyEncryptedData encryptedData = null;
-    
+
         // Find the encrypted data for the correct private key
         for (Iterator<PGPEncryptedData> it = encryptedDataList.getEncryptedDataObjects(); it.hasNext(); ) {
             PGPEncryptedData pgpEncryptedData = it.next();
-    
-            // Check if it's an instance of PGPPublicKeyEncryptedData
+
             if (pgpEncryptedData instanceof PGPPublicKeyEncryptedData) {
                 PGPPublicKeyEncryptedData publicKeyEncryptedData = (PGPPublicKeyEncryptedData) pgpEncryptedData;
-    
-                // Match the encrypted data with the provided private key
+
                 if (publicKeyEncryptedData.getKeyID() == privateKey.getKeyID()) {
                     encryptedData = publicKeyEncryptedData;
                     break;
                 }
             }
         }
-    
+
         if (encryptedData == null) {
             throw new IllegalArgumentException("No encrypted data found for the provided private key.");
         }
-    
+
         try {
             // Decrypt the file
             InputStream clearData = encryptedData.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder()
                     .setProvider("BC")
                     .build(privateKey));
-    
-            PGPObjectFactory plainFactory = new PGPObjectFactory(clearData, new JcaKeyFingerprintCalculator());
+
+            JcaPGPObjectFactory plainFactory = new JcaPGPObjectFactory(clearData);
             Object message = plainFactory.nextObject();
-    
-            // Handle PGPMarker, which can appear in some PGP streams
-            while (message instanceof PGPMarker) {
-                System.out.println("PGPMarker found, moving to the next object...");
-                message = plainFactory.nextObject(); // Skip over marker
-            }
-    
-            System.out.println("Object returned by PGPObjectFactory: " + message.getClass().getName());
-    
-            // Dealing with compressed data
+
+            // Handle PGPCompressedData
             if (message instanceof PGPCompressedData) {
                 PGPCompressedData compressedData = (PGPCompressedData) message;
-                plainFactory = new PGPObjectFactory(compressedData.getDataStream(), new JcaKeyFingerprintCalculator());
+                plainFactory = new JcaPGPObjectFactory(compressedData.getDataStream());
                 message = plainFactory.nextObject();
             }
-    
+
             // Dealing with literal data
             if (message instanceof PGPLiteralData) {
                 PGPLiteralData literalData = (PGPLiteralData) message;
                 try (InputStream literalInput = literalData.getInputStream()) {
-                    byte[] buffer = new byte[4096];
-                    int len;
-                    while ((len = literalInput.read(buffer)) > 0) {
-                        outStream.write(buffer, 0, len);
-                    }
+                    Streams.pipeAll(literalInput, outStream);
                 }
             } else {
                 throw new PGPException("Message is not a literal data packet.");
             }
-    
         } catch (Exception e) {
             throw new PGPException("Error during decryption: " + e.getMessage(), e);
         }
     }
 
-    
     public static void main(String[] args) throws Exception {
         String inputFilePath = "encrypted_sample.txt";
         String outputFilePath = "decrypted_sample.txt";
--- rnd/src/PgpKeysDemo/readme.txt
+++ rnd/src/PgpKeysDemo/readme.txt
@@ -2,6 +2,8 @@
 https://github.com/marlanfernando/pgpUtils/blob/main/src/main/java/util/PgpPainlessUtil.java
 https://medium.com/@marlanfernando1991/pretty-good-privacy-pgp-painless-encryption-decryption-with-java-0a9236d034ea
 https://github.com/GregorySoto/pgp-encryption-using-java
+https://didisoft.com/java-openpgp/examples/decrypt/
+
 
 https://www.youtube.com/watch?v=J9nTWWnME-s
 one pgp
