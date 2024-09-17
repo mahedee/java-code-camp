090064396d48c0c3623d63990e8fcff37270274d
mahedee <mahedee.hasan@gmail.com>
2024-09-17 16:31:33 -0400

    ....

--- rnd/src/PGP/mpgp-decryptor-shellscripts/decrypt-file.sh
+++ rnd/src/PGP/mpgp-decryptor-shellscripts/decrypt-file.sh
@@ -4,7 +4,9 @@
 SOURCE_PATH="//opt/app/source"
 DESTINATION_PATH="//opt/app/dest"
 PRIVATE_KEY_PATH="//opt/app/pgp"
-PASSPHRASE="strong-passphrase"
+PATH_FOR_BAD_FILES="//opt/app/bad"
+
+#PASSPHRASE="strong-passphrase"
 
 # JAR file which will be run
 JARFILENAME="mpgpdecryptor-1.0-SNAPSHOT-jar-with-dependencies.jar"
@@ -13,10 +15,11 @@ JARFILENAME="mpgpdecryptor-1.0-SNAPSHOT-jar-with-dependencies.jar"
 echo "Source Path: $SOURCE_PATH"
 echo "Destination Path: $DESTINATION_PATH"
 echo "PGP File Path: $PRIVATE_KEY_PATH"
-echo "Passphrase: $PASSPHRASE"
+echo "Bad files Path: $PATH_FOR_BAD_FILES"
+#echo "Passphrase: $PASSPHRASE"
 
 # Command to run jar
-java -jar $JARFILENAME $SOURCE_PATH $DESTINATION_PATH $PRIVATE_KEY_PATH $PASSPHRASE
+java -jar $JARFILENAME $SOURCE_PATH $DESTINATION_PATH $PRIVATE_KEY_PATH $PATH_FOR_BAD_FILES
 
 
 # Further logic for file transfer, encryption, etc. can go here.
